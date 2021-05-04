038fed7a3cc616e2941b79ddd9f121898537810b
etiennezastko <etiennezastko@hotmail.fr>
2021-05-04 14:29:20 +0200

    update

--- index.php
+++ index.php
@@ -12,7 +12,20 @@
 <body>
 
     <script src="main.js"></script>
-    <div class="menu"></div>
+    <div class="row"></div>
+    <div class="nav">
+
+
+        <a href="Acceuil" class="item_1">Acceuil</a>
+        <a href="contact" class="item_2">contact</a>
+        <a href="à propos" class="item_3">à propos</a>
+        <img src="5.png" alt="5.png" class="button">
+
+
+
+    </div>
+
+
 </body>
 
 </html>
