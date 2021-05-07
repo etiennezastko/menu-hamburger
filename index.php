87347a0a34a6d229a344237d505c0539b6b52e66
etiennezastko <etiennezastko@hotmail.fr>
2021-05-07 09:03:43 +0200

    first commit

--- index.php
+++ index.php
@@ -11,20 +11,26 @@
 
 <body>
 
-    <script src="main.js"></script>
-    <div class="row"></div>
+
     <div class="nav">
+        <img src="5.png" alt="5.png" class="button" id="btn" />
+
+        <div href="Acceuil" id="nav" class="item_1  item">
+            <a href="contact" class="item_2">Acceuil</a>
+            <a href="contact" class="item_2">contact</a>
+            <a href="à propos" class="item_3">à propos</a>
 
 
-        <a href="Acceuil" class="item_1">Acceuil</a>
-        <a href="contact" class="item_2">contact</a>
-        <a href="à propos" class="item_3">à propos</a>
-        <img src="5.png" alt="5.png" class="button">
-
-
-
+        </div>
     </div>
 
+    <div class="menu_items hidden" id="navmobile">
+        <a href="Acceuil" class="item-1">Acceuil
+        </a>
+        <a href="Acceuil" class="item-1">contact</a>
+        <a class="item-1">à propos</a>
+        </a>
+        <script src="main.js"></script>
 
 </body>
 
---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById('btn').addEventListener('click',toggleBurgerMenu);
+    
+
+function toggleBurgerMenu(){
+
+    
+
+    
+    document.getElementById('nav').classList.toggle
+    ('hidden');
+    document.getElementById('navmobile').classList.toggle('hidden');
+};
+
+
+
+
